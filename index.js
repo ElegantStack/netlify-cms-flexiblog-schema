ad01e1904402aadbb1088f8aa35e367be14ee7b9
ElegantStack <elegantstack.studio@gmail.com>
2021-09-03 10:49:30 -0700

    copy config file from package

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 #!/usr/bin/env node
 
-const https = require('https')
-const fs = require('fs')
+var path = require('path')
+var fs = require('fs')
 
-var dir = './static/admin'
 var fileName = 'config.yml'
+var destinationDir = path.join(process.cwd(), 'static', 'admin')
+var destinationFile = path.join(destinationDir, fileName)
+var sourceFile = path.join(__dirname, fileName)
 
-if (!fs.existsSync(dir)) {
-	fs.mkdirSync(dir, { recursive: true })
+if (!fs.existsSync(destinationDir)) {
+	fs.mkdirSync(destinationDir, { recursive: true })
 }
 
-const file = fs.createWriteStream(dir + '/' + fileName)
-const request = https.get(
-	'https://raw.githubusercontent.com/ElegantStack/gatsby-recipes/master/src/flexiblog-netlify-cms/config.yml',
-	function (response) {
-		response.pipe(file)
-	}
-)
+fs.copyFile(sourceFile, destinationFile, (err) => {
+	if (err) throw err
+	console.log(
+		'\x1b[32m%s\x1b[0m',
+		'Successfully created Netlify configuration at:\n',
+		'\x1b[1m',
+		destinationFile
+	)
+})
